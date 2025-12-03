c59376e0ce311aadaeb9413ad3468509be445473
Srajan <Srajan@LAPTOP-SKIRSBOL>
2025-12-03 17:31:54 +0530

    My first commit

---
+++ ProjectForMavenGitHubJenkinsPractice/src/test/java/package1/Class1Test.java
@@ -0,0 +1,15 @@
+package package1;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Class1Test {
+
+	@Test
+	public void SampleTest() {
+		
+		Reporter.log("........Hii.......",true);
+		System.out.println("*************Hiiii******************");
+
+	}
+}
